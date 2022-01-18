c1da0ed3704675ba6a021f9522d9627a6b356e34
n2ptune <n2ptune@outlook.com>
2022-01-18 20:33:14 +0900

    vite ssg test

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+export const createApp = ViteSSG(
+  App,
+  {
+    routes: [
+      {
+        path: '/',
+        name: 'main',
+        component: () => import('./components/HelloWorld.vue')
+      }
+    ]
+  },
+  ({ app, router, routes, isClient, initialState }) => {
+    console.log(app)
+  }
+)
